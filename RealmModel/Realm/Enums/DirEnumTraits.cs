5a94acddbfc7d218964447e3e3f95948697d36ae
fwhaslam <fwhaslam@gmail.com>
2021-11-07 19:26:27 -0800

    DirEnumTraits expanded to support GridStepper.

--- RealmModel/Realm/Enums/DirEnumTraits.cs
+++ RealmModel/Realm/Enums/DirEnumTraits.cs
@@ -5,27 +5,61 @@
 namespace Realm.Enums {
 
 	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+
+	public class DirEnumTrait {
+
+		public DirEnumTrait( int dx,int dy,bool orth,int stp ) {
+			this.DX = dx;
+			this.DY = dy;
+			this.Orth = orth;
+			this.Steps = stp;
+		}
+		public int DX {  get; internal set; }
+		public int DY {  get; internal set; }
+
+		public bool Orth { get; internal set; }
+		public int Steps {  get; internal set; }
+
+		public string ToDisplay() {
+			return "DirEnumTrait(DX="+DX+",DY="+DY+",Steps="+Steps+")";
+		}
+
+	}
 
 	public class DirEnumTraits {
 
-		static Tuple<int,int>[] delta = { 
-			new Tuple<int,int>(0,1),
-			new Tuple<int,int>(1,1),
-			new Tuple<int,int>(1,0),
-			new Tuple<int,int>(1,-1),
+		static readonly DirEnumTrait[] traits = {
+			new DirEnumTrait( 0, 1, true, 2 ),	// north
+			new DirEnumTrait( 1, 1, false,  3 ),
+			new DirEnumTrait( 1, 0, true, 2 ),
+			new DirEnumTrait( 1, -1, false, 3 ),
 
-			new Tuple<int,int>(0,-1),
-			new Tuple<int,int>(-1,-1),
-			new Tuple<int,int>(-1,0),
-			new Tuple<int,int>(-1,1) };
+			new DirEnumTrait( 0, -1, true, 2 ),	// south
+			new DirEnumTrait( -1, -1, false, 3 ),
+			new DirEnumTrait( -1, 0, true, 2 ),
+			new DirEnumTrait( -1, 1, false, 3 )
 
-		static public int Count() { return Enum.GetNames(typeof(DirEnum)).Length; }
+		};
 
-		static public int DX(DirEnum dir) { return delta[(int)dir].Item1; }
+		static public readonly int Count =  Enum.GetNames(typeof(DirEnum)).Length; 
 
-		static public int DY(DirEnum dir) { return delta[(int)dir].Item2; }
+		static public readonly IEnumerable<int> Range = Enumerable.Range(0,Count);
 
-		static public Tuple<int,int> Delta(DirEnum dir) { return delta[(int)dir]; }
+		static public int DX(DirEnum dir) { return traits[(int)dir].DX; }
+
+		static public int DY(DirEnum dir) { return traits[(int)dir].DY; }
+
+		static public bool Orth(DirEnum dir) { return traits[(int)dir].Orth; }
+
+		static public int Steps(DirEnum dir) { return traits[(int)dir].Steps; }
+
+		static public DirEnumTrait Trait(DirEnum dir) { return traits[(int)dir]; }
+
+		static public DirEnumTrait[]  All {  
+			get {return traits; }
+		}
 
 	}
 
---
+++ RealmModelTests/Realm/Enums/DirEnumTraitsTest.cs
@@ -0,0 +1,78 @@
+﻿//
+//	Copyright 2021 Frederick William Haslam born 1962 in the USA
+//
+
+namespace Realm.Enums {
+
+	using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+
+	using static Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+	[TestClass]
+	public class DirEnumTraitsTest {
+
+		[TestMethod]
+		public void DX() { 
+		
+			AreEqual( 0, DirEnumTraits.DX( DirEnum.North) );
+			AreEqual( 1, DirEnumTraits.DX( DirEnum.NorthEast) );
+			AreEqual( 1, DirEnumTraits.DX( DirEnum.East) );
+			AreEqual( 1, DirEnumTraits.DX( DirEnum.SouthEast) );
+
+			AreEqual( 0, DirEnumTraits.DX( DirEnum.South) );
+			AreEqual( -1, DirEnumTraits.DX( DirEnum.SouthWest) );
+			AreEqual( -1, DirEnumTraits.DX( DirEnum.West) );
+			AreEqual( -1, DirEnumTraits.DX( DirEnum.NorthWest) );
+		}
+
+		[TestMethod]
+		public void DY() { 
+		
+			AreEqual( 1, DirEnumTraits.DY( DirEnum.North) );
+			AreEqual( 1, DirEnumTraits.DY( DirEnum.NorthEast) );
+			AreEqual( 0, DirEnumTraits.DY( DirEnum.East) );
+			AreEqual( -1, DirEnumTraits.DY( DirEnum.SouthEast) );
+
+			AreEqual( -1, DirEnumTraits.DY( DirEnum.South) );
+			AreEqual( -1, DirEnumTraits.DY( DirEnum.SouthWest) );
+			AreEqual( 0, DirEnumTraits.DY( DirEnum.West) );
+			AreEqual( 1, DirEnumTraits.DY( DirEnum.NorthWest) );
+		}
+
+		[TestMethod]
+		public void Orth() { 
+		
+			AreEqual( true, DirEnumTraits.Orth( DirEnum.North) );
+			AreEqual( false, DirEnumTraits.Orth( DirEnum.NorthEast) );
+			AreEqual( true, DirEnumTraits.Orth( DirEnum.East) );
+			AreEqual( false, DirEnumTraits.Orth( DirEnum.SouthEast) );
+
+			AreEqual( true, DirEnumTraits.Orth( DirEnum.South) );
+			AreEqual( false, DirEnumTraits.Orth( DirEnum.SouthWest) );
+			AreEqual( true, DirEnumTraits.Orth( DirEnum.West) );
+			AreEqual( false, DirEnumTraits.Orth( DirEnum.NorthWest) );
+		}
+
+		[TestMethod]
+		public void Steps() { 
+		
+			AreEqual( 2, DirEnumTraits.Steps( DirEnum.North) );
+			AreEqual( 3, DirEnumTraits.Steps( DirEnum.NorthEast) );
+			AreEqual( 2, DirEnumTraits.Steps( DirEnum.East) );
+			AreEqual( 3, DirEnumTraits.Steps( DirEnum.SouthEast) );
+
+			AreEqual( 2, DirEnumTraits.Steps( DirEnum.South) );
+			AreEqual( 3, DirEnumTraits.Steps( DirEnum.SouthWest) );
+			AreEqual( 2, DirEnumTraits.Steps( DirEnum.West) );
+			AreEqual( 3, DirEnumTraits.Steps( DirEnum.NorthWest) );
+		}
+
+
+	}
+}
